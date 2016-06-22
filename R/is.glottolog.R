4d8532de959a3be2467f6abb1da13653a193a5fa
agricolamz <agricolamz@gmail.com>
2016-06-22 23:00:53 +0300

    is.glottolog with spellchecker

--- R/is.glottolog.R
+++ R/is.glottolog.R
@@ -1,8 +1,19 @@
-#' Is it in glottolog?
+#' Are these langoids in glottolog?
 #'
 #' Takes any vector of linguoids or ISO codes and return a logical vector.
 #' @param x A character vector of linguoids (can be written in lower case)or ISO codes
+#' @param response logical. If TRUE, when languoid is absent, return warnings with a possible candidates.
 #' @author George Moroz <agricolamz@gmail.com>
+#' @examples
+#' is.glottolog(c("Adyghe", "Russsian")
+#' TRUE FALSE
+#'
+#' is.glottolog(c("Adyghe", "Russsian", response = T)
+#' TRUE FALSE
+#' Warning:
+#' In is.glottolog(c("Adyghe", "Russsian"), response = T) :
+#' Languoid Russsian is absent in our database. Did you mean Russian?
+#'
 #' @export
 #' @import stringdist
 
