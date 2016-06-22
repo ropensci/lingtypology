cfd74ba568dc545d292884192768ad3d08a8cfd4
agricolamz <agricolamz@gmail.com>
2016-06-22 22:52:43 +0300

    is.glottolog with spellchecker

--- R/is.glottolog.R
+++ R/is.glottolog.R
@@ -4,11 +4,27 @@
 #' @param x A character vector of linguoids (can be written in lower case)or ISO codes
 #' @author George Moroz <agricolamz@gmail.com>
 #' @export
+#' @import stringdist
 
-is.glottolog <- function(x){
+is.glottolog <- function(x, response = F){
+if(response == T) {
+result <- NA
+    for (i in 1:length(x)) {
+    result[i] <- sum(tolower(glottolog$languoid)==tolower(x[i]) | tolower(glottolog$iso)==tolower(x[i])) > 0
+    if (result[i] == FALSE) {
+      cand <- stringdist::stringdist(x[i], glottolog$languoid, method = "lv")
+      candidates <- paste(glottolog[cand == cand[which.min(cand)],]$languoid, collapse = ", ")
+      warning(paste("Languoid ", x[i], " is absent in our database. Did you mean ",
+                    candidates, "?", sep = ""))
+      x <- c(4, 4, 9, 9)
+      x[x == x[which.min(x)]]
+      which.min(x)
+    }
+  }
+} else {
 result <- NA
   for (i in 1:length(x)) {
   result[i] <- sum(tolower(glottolog$languoid)==tolower(x[i]) | tolower(glottolog$iso)==tolower(x[i])) > 0
-  }
+  }}
 return(result)
 }
