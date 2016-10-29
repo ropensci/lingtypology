97946a8f6fde0062f25352a26a88642943fecd22
agricolamz <agricolamz@gmail.com>
2016-10-29 12:29:30 +0300

    turn for loops into sapply

--- R/lang.aff.R
+++ R/lang.aff.R
@@ -2,11 +2,12 @@
 #'
 #' Takes any vector of affiliations and return languoids.
 #' @param x A character vector of the affiliations (can be written in lower case)
-#' @param list logical. If TRUE, returns a list of languoids, if ELSE return a named vector.
+#' @param list logical. If TRUE, returns a list of languoids, if FALSE return a named vector.
 #' @author George Moroz <agricolamz@gmail.com>
 #' @examples
 #' lang.aff("Balto-Slavic")
 #' lang.aff(c("East Slavic", "West Slavic"))
+#' lang.aff(c("East Slavic", "West Slavic"), list = TRUE)
 #' @export
 
 lang.aff <- function(x, list = FALSE){
--- R/lang.country.R
+++ R/lang.country.R
@@ -2,30 +2,24 @@
 #'
 #' Takes any vector of countries and return languoids.
 #' @param x character vector of the countries (can be written in lower case)
+#' @param list logical. If TRUE, returns a list of languoids, if FALSE return a vector.
 #' @examples
 #' lang.country("Bali")
 #' lang.country(c("Bali", "Luxembourg"))
+#' lang.country(c("Bali", "Luxembourg"), list = TRUE)
 #' ## What languoids are both in North Korea and in South Korea?
 #' lang.country("Korea")
 #' @author George Moroz <agricolamz@gmail.com>
 #' @export
 
-lang.country <- function(x){
-  input <- tolower(x)
-  a <- NA
-  for (i in 1:length(input)) {
-# abbreviation -----------------------------------------
-    if (sum(input[i]==tolower(countries$abbreviation)) > 0) {
-      input[i]<- tolower(as.character(countries$common[input[i]==tolower(countries$abbreviation)]))
-    }
-# common -----------------------------------------
-    if (sum(input[i]==tolower(countries$official)) > 0) {
-      input[i]<- tolower(as.character(countries$common[input[i]==tolower(countries$official)]))
-    }
-# get country -------------------------------------------------------------
-    a <- c(a, glottolog[grepl(input[i], tolower(glottolog$country)),]$languoid)
-  }
-  ret <- a[complete.cases(a)]
-  if (length(ret) < 1) {
-    warning('There is no such a country')
-  } else {ret}}
+lang.country <- function(x, list = FALSE){
+  ret <- lapply(x, function(y){
+    cntr <- countries$common[(tolower(countries$common) %in% tolower(y) |
+                                tolower(countries$official) %in% tolower(y) |
+                                tolower(countries$abbreviation) %in% tolower(y))]
+    if(length(cntr) > 0){
+      glottolog[grep(cntr, glottolog$country),]$languoid}
+    else{NA}})
+  if(list == TRUE){
+    return(ret)} else {
+      return(unlist(ret))}}
