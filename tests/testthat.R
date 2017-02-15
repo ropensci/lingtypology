353f7cb4081fa2260a687e5cb0ddd4549af43252
agricolamz <agricolamz@gmail.com>
2017-02-15 17:44:31 +0300

    about factor

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(lingtypology)
-
-test_check("lingtypology")
--- tests/testthat/test-makelink.R
+++
@@ -1,8 +0,0 @@
-library(lingtypology)
-context("Tests for makelink function")
-
-test_that("makelink", {
-  expect_equal(makelink("Adyghe"),
-               "<a href='http://glottolog.org/resource/languoid/iso/ady' target='_blank'>Adyghe</a><br>"
-  )
-})
