0fef4c7aefe97a8e733d7cb862d8ef4bdc3d7da3
George Moroz <agricolamz@gmail.com>
2020-10-03 00:03:23 +0300

    update glottolog

--- tests/testthat/test-aff-lang.R
+++ tests/testthat/test-aff-lang.R
@@ -1,10 +1,7 @@
-library(lingtypology)
-context("Tests for aff.lang function")
-
 df <- data.frame(my_langs = c("Korean", "Polish"), stringsAsFactors = FALSE)
 
 test_that("aff.lang", {
     expect_equal(aff.lang("Korean"), c(Korean = "Koreanic"))
-    expect_equal(aff.lang(c("Korean", "Polish")), c(Korean = "Koreanic", Polish = "Indo-European,Balto-Slavic,Slavic,West Slavic,Lechitic"))
-    expect_equal(aff.lang(df), c(my_langs1 = "Koreanic", my_langs2 = "Indo-European,Balto-Slavic,Slavic,West Slavic,Lechitic"))
+    expect_equal(aff.lang(c("Korean", "Polish")), c(Korean = "Koreanic", Polish = "Indo-European,Classical Indo-European,Balto-Slavic,Slavic,West Slavic,Lechitic"))
+    expect_equal(aff.lang(df), c(my_langs1 = "Koreanic", my_langs2 = "Indo-European,Classical Indo-European,Balto-Slavic,Slavic,West Slavic,Lechitic"))
 })
