e45351b1edc06a1c806cb1b34f71034688a7f69e
forlinguistics <42992800+forlinguistics@users.noreply.github.com>
2020-12-12 13:28:36 +0300

    deleted views

--- R/sails.feature.R
+++ R/sails.feature.R
@@ -1,7 +1,8 @@
 #' Download SAILS data
 #'
 #' This function downloads data from SAILS (\url{https://sails.clld.org/}) and changes language names to the names from lingtypology database. You need the internet connection.
-#' @author 
+#' @author
+
 #' @seealso \code{\link{abvd.feature}}, \code{\link{afbo.feature}}, \code{\link{autotyp.feature}}, \code{\link{oto_mangueanIC.feature}}, \code{\link{phoible.feature}}, \code{\link{uralex.feature}}, \code{\link{valpal.feature}}, \code{\link{wals.feature}}
 #' @examples
 #' # sails.feature()
@@ -15,14 +16,14 @@ values<-values[,c('ID','Language_ID','Code_ID','Source','Contributor')]
 
 langs<-read.csv('https://raw.githubusercontent.com/cldf-datasets/sails/master/cldf/languages.csv')
 langs<-langs[,c("ID","Name","Latitude","Longitude","Glottocode")]
-langs$language_for_lingtypology <- lang.gltc(langs$Glottocode)
-codes<-read.csv('https://raw.githubusercontent.com/cldf-datasets/sails/master/cldf/codes.csv') 
+langs$language_for_lingtypology <- lingtypology::lang.gltc(langs$Glottocode)
+codes<-read.csv('https://raw.githubusercontent.com/cldf-datasets/sails/master/cldf/codes.csv')
 
 m1<-merge(values,langs,by.x = 'Language_ID', by.y = 'ID')
 m1 %>% View()
 
 final_df<- merge(m1,codes, by.x='Code_ID',by.y = 'ID')
-final_df<-final_df[,c('ID','Name.x',"Latitude","Longitude","language_for_lingtypology","Parameter_ID","Name.y","Description")] 
+final_df<-final_df[,c('ID','Name.x',"Latitude","Longitude","language_for_lingtypology","Parameter_ID","Name.y","Description")]
 
 names(final_df)[names(final_df) == "Name.x"] <- "lang_name"
 names(final_df)[names(final_df) == "Name.y"] <- "Value"
