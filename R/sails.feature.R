da84cea4f38bf0221bad9f942a1351ca9cc66e1c
forlinguistics <42992800+forlinguistics@users.noreply.github.com>
2020-12-12 13:39:19 +0300

    deleted Views

--- R/sails.feature.R
+++ R/sails.feature.R
@@ -20,11 +20,9 @@ langs$language_for_lingtypology <- lingtypology::lang.gltc(langs$Glottocode)
 codes<-read.csv('https://raw.githubusercontent.com/cldf-datasets/sails/master/cldf/codes.csv')
 
 m1<-merge(values,langs,by.x = 'Language_ID', by.y = 'ID')
-m1 %>% View()
 
 final_df<- merge(m1,codes, by.x='Code_ID',by.y = 'ID')
 final_df<-final_df[,c('ID','Name.x',"Latitude","Longitude","language_for_lingtypology","Parameter_ID","Name.y","Description")]
 
 names(final_df)[names(final_df) == "Name.x"] <- "lang_name"
 names(final_df)[names(final_df) == "Name.y"] <- "Value"
-final_df %>% View
