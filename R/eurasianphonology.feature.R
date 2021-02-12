0602ffad765f7233e75c613e95b9a736c82ad6a6
kirillkoncha <majortomblog@gmail.com>
2021-02-12 19:23:40 +0300

    final commit i hope

--- R/eurasianphonology.feature.R
+++ R/eurasianphonology.feature.R
@@ -1,15 +1,16 @@
-#' Open .csv file of The database of Eurasian phonological inventories (beta version) which was downloaded via eurasianphonology.R script.
+#' Download data from the database of Eurasian phonological inventories
 #'
 #' This function open downloaded data from the database of Eurasian phonological inventories (\url{http://eurasianphonology.info}) and changes language names to the names from lingtypology database. You need the internet connection.
 #'
 #' @author Kirill Koncha <majortomblog@gmail.com>
-#' @seealso \code, \code{\link{afbo.feature}}, \code{\link{autotyp.feature}}, \code{\link{oto_mangueanIC.feature}}, \code{\link{phoible.feature}}, \code{\link{sails.feature}}, \code{\link{valpal.feature}}, \code{\link{wals.feature}}
+#' @seealso \code{\link{afbo.feature}}, \code{\link{autotyp.feature}}, \code{\link{oto_mangueanIC.feature}}, \code{\link{phoible.feature}}, \code{\link{sails.feature}}, \code{\link{valpal.feature}}, \code{\link{wals.feature}}
 #' @examples
-#' eurasianphonolgy.feature
+#'
+#' eurasianphonology.feature()
 #'
 #' @export
 #'
-eurasianphonolgy.feature <- function(){
+eurasianphonology.feature <- function(){
   message("Don't forget to cite a source:
 
 Dmitry Nikolaev (ed.), Andrey Nikulin, Anton Kukhto. 2020. The database of Eurasian phonological inventories (beta version) (Available online at http://eurasianphonology.info, Accessed on ...)")
--- tests/testthat/test-eurasianphonology-feature.R
+++ tests/testthat/test-eurasianphonology-feature.R
@@ -2,4 +2,4 @@ library(testthat)
 context("Tests for eurasianphonology.feature function")
 
 test_that("eurasianphonology.feature",  {
-  expect_length(eurasianphonolgy.feature(), 19)})
+  expect_length(eurasianphonology.feature(), 19)})
