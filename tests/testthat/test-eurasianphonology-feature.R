0c0ff69dc68dd43935f9f653d0b5a2a45f34f418
kirillkoncha <42929201+kirillkoncha@users.noreply.github.com>
2021-02-12 16:57:24 +0300

    Add files via upload

---
+++ tests/testthat/test-eurasianphonology-feature.R
@@ -0,0 +1,6 @@
+library(lingtypology)
+library(testthat)
+context("Tests for eurasianphonology.feature function")
+
+test_that("eurasianphonology.feature",  {
+  expect_length(eurasianphonology.feature(), 19)})
