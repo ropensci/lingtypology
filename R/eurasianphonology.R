1f7b42c44c85007c71a597eb8a994227edf99a64
kirillkoncha <42929201+kirillkoncha@users.noreply.github.com>
2021-02-12 16:47:19 +0300

    Add files via upload

---
+++ R/eurasianphonology.R
@@ -0,0 +1,28 @@
+#' Eurasianphonology data
+#'
+#' Data from The database of Eurasian phonological inventories (\url{http://eurasianphonology.info}). This dataset is created for \code{\link{eurasianphonology.feature}} function.
+#'
+#' #' @format A data frame with 19825 rows and 18 variables:
+#' \describe{
+#'   \item{id}{Language id}
+#'   \item{code}{ISO code}
+#'   \item{type}{Language or dialect}
+#'   \item{name}{Language name}
+#'   \item{coords1}{latitude}
+#'   \item{coords2}{longitude}
+#'   \item{gen1}{Language Family}
+#'   \item{gen2}{Language Family}
+#'   \item{tones}{Inventory of tones}
+#'   \item{syllab}{Syllab structure}
+#'   \item{cluster}{Cluster}
+#'   \item{finals}{Finals}
+#'   \item{source}{Source}
+#'   \item{comment}{Comment}
+#'   \item{Contr}{Contributor}
+#'   \item{Segment type}{Vowels or consonants}
+#'   \item{segments}{Segments}
+#'   \item{glottocode}{Glottocode}
+#' }
+#'
+
+"eurasianphonology"
