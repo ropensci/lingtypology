b87ef854ce88821a286f51b748f13c35552e24fc
forlinguistics <42992800+forlinguistics@users.noreply.github.com>
2021-02-14 22:50:41 +0300

    some more

--- R/vanuatu.feature.R
+++ R/vanuatu.feature.R
@@ -8,30 +8,7 @@
 #' @export
 #' @importFrom  utils read.csv
 #' @importFrom jsonlite fromJSON
-add_param<-function(x){
-  parameter_json <- jsonlite::fromJSON(paste0('https://vanuatuvoices.clld.org/parameters/', x, '.geojson'),flatten = TRUE)
-  parameters<-parameter_json$features
-  words<-vector()
-  audios<-vector()
-  for (feature in parameters$properties.values){
-    if (nrow(feature)>1){
-      word_forms<-paste(feature$name[!is.na(feature$name)],collapse = ', ')
-      aud_var<-paste(feature$jsondata.audio[!is.na(feature$jsondata.audio)],collapse = ', ')
-      words<-c(words,word_forms)
-      audios<-c(audios,aud_var)
-    }
-    else{
-      words<-c(words,feature$name)
-      audios<-c(audios,feature$jsondata.audio)
-    }
-  }
-  parameters['word']<-words
-  parameters['audio']<-audios
-  parameters['feature']<-c(rep(x,length(words)))
-  parameters<-parameters[,c('feature','word','audio','properties.language.name','properties.language.description','properties.language.island','properties.language.latitude','properties.language.longitude','properties.language.glottocode')]
-  names(parameters)<-c('feature','word','audio','language_name','language_description','island','latitude','longitude','glottocode')
-  return(parameters)
-}
+
 
 vanuatu.feature <-function(features,na.rm = TRUE) {
 
@@ -53,9 +30,33 @@ Aviva Shimelman, Mary Walworth, Lana Takau, Tom Ennever, Iveth Rodriguez, Tom Fi
       "in vanuatu database."
     ))
   }
-  attempt(e_features<-as.character(match.arg(features, feature_list,several.ok = TRUE)),msg = '')
+  e_features<-as.character(match.arg(features, feature_list,several.ok = TRUE))
   e_features<-as.character(match.arg(features, feature_list,several.ok = TRUE))
   e_ids<-id_list[match(e_features,feature_list)]
+  add_param<-function(x){
+    parameter_json <- jsonlite::fromJSON(paste0('https://vanuatuvoices.clld.org/parameters/', x, '.geojson'),flatten = TRUE)
+    parameters<-parameter_json$features
+    words<-vector()
+    audios<-vector()
+    for (feature in parameters$properties.values){
+      if (nrow(feature)>1){
+        word_forms<-paste(feature$name[!is.na(feature$name)],collapse = ', ')
+        aud_var<-paste(feature$jsondata.audio[!is.na(feature$jsondata.audio)],collapse = ', ')
+        words<-c(words,word_forms)
+        audios<-c(audios,aud_var)
+      }
+      else{
+        words<-c(words,feature$name)
+        audios<-c(audios,feature$jsondata.audio)
+      }
+    }
+    parameters['word']<-words
+    parameters['audio']<-audios
+    parameters['feature']<-c(rep(x,length(words)))
+    parameters<-parameters[,c('feature','word','audio','properties.language.name','properties.language.description','properties.language.island','properties.language.latitude','properties.language.longitude','properties.language.glottocode')]
+    names(parameters)<-c('feature','word','audio','language_name','language_description','island','latitude','longitude','glottocode')
+    return(parameters)
+  }
   final_df_list<-lapply(e_ids,add_param)
   final_df<-do.call(rbind, final_df_list)
   if (na.rm){
