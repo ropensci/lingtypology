6e49354b62ce4572640c7b839e08355b9d9b7298
George Moroz <agricolamz@gmail.com>
2021-10-24 17:11:50 +0300

    fix  and ; hopefully close #69

--- tests/testthat/test-soundcomparisons-feature.R
+++ tests/testthat/test-soundcomparisons-feature.R
@@ -2,5 +2,5 @@ library(lingtypology)
 context("Tests for soundcomparisons.feature function")
 
 test_that("soundcomparisons.feature", {
-  expect_error(soundcomparisons.feature(c("aa", "bb")), "There is no features 'aa' in soundcomparisons database.")
+  expect_error(soundcomparisons.feature(c("aa", "bb")), "There is no features 'aa', 'bb' in our version of the soundcomparisons database.")
 })
