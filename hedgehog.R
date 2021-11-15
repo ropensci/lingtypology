87f1a28db8d62b38159351b9f288f8ab6738d5d6
ustera <36166531+ustera@users.noreply.github.com>
2021-11-15 22:41:44 +0300

    add description

--- hedgehog.R
+++ hedgehog.R
@@ -1,3 +1,14 @@
+#' Sounds of a hedgehog
+#'
+#' This function reproduces the sounds of a hedgehog. You don't need the internet connection.
+#'
+#' @param feature times number of repetitions.
+#' @author Ekaterina Zalivina <zalivina01@mail.ru>
+#' @seealso \code{\link{afbo.feature}}, \code{\link{autotyp.feature}}, \code{\link{bivaltyp.feature}}, \code{\link{eurasianphonology.feature}}, \code{\link{oto_mangueanIC.feature}}, \code{\link{phoible.feature}}, \code{\link{sails.feature}}, \code{\link{soundcomparisons.feature}}, \code{\link{uralex.feature}}, \code{\link{valpal.feature}}, \code{\link{vanuatu.feature}}, \code{\link{wals.feature}}
+#' @examples
+#' # hedgehog()
+#' @export
+
 hedgehog <- function(times){
   sound <- "фыр"
   text <- rep(sound, times)
