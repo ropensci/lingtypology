7af285d9f02ac2dd62f3d2df51241dd4c63e1587
kirillkoncha <majortomblog@gmail.com>
2021-11-16 17:10:30 +0300

    WCS updated

---
+++ R/wcs_bk.R
@@ -0,0 +1,23 @@
+#' WCS data
+#'
+#' Data from The database of WCS Data Archives (\url{https://www1.icsi.berkeley.edu/wcs/data.html}). This dataset is created for \code{\link{wcs.feature}} function.
+#'
+#' @format A data frame with 32211 rows and 13 variables:
+#' \describe{
+#'   \item{language_id}{Language id}
+#'   \item{speaker_id}{Speaker id}
+#'   \item{term_abbr}{Color term abbriviation}
+#'   \item{language_original}{Language name in the original dataset}
+#'   \item{language}{Language name in lingtypology}
+#'   \item{iso}{ISO code}
+#'   \item{glottocode}{Glottocode}
+#'   \item{longitude}{Longitude}
+#'   \item{latitude}{Latitude}
+#'   \item{focus_response}{Focus response}
+#'   \item{grid_coordinates}{Grid coordinates}
+#'   \item{term_id}{Color term id}
+#'   \item{term_transcription}{Color term transcription}
+#' }
+#
+
+"wcs_bk"
---
+++ tests/testthat/test-wcs-feature.R
@@ -0,0 +1,5 @@
+library(testthat)
+context("Tests for wcs.feature function")
+
+test_that("wcs.feature",  {
+  expect_length(wcs.feature("WCS"), 19)})
